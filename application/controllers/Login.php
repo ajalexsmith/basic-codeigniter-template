0c4ea66d31eaf89a9f9e507b42e35fa3ca3dbb0f
flapjack321 <31697174+flapjack321@users.noreply.github.com>
2017-09-06 21:44:31 +0100

    Add files via upload

---
+++ application/controllers/Login.php
@@ -0,0 +1,25 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+	public function __construct() {
+		parent::__construct();
+		$this->load->helper("form");
+		$this->load->model("login_model");
+	}
+	public function index($error = "") {
+		$this->session->sess_destroy();
+		$data["page"] = "Login";
+		$data["error"] = $error;
+		$this->load->view("login", $data);
+	}
+	public function check() {
+		$username = $this->input->post("username", true);
+		$password = $this->input->post("password", true);
+		$valid = $this->login_model->validate($username, $password);
+		if ($valid === true) {
+			redirect("/home/", "refresh");
+		} else {
+			$this->index($valid);
+		}
+	}
+}
